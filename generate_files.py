f3c21e565f5f71f211907a033fff4da7b00401be
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-04-17 12:50:39 -0400

    Updated generate_files.py

--- generate_files.py
+++ generate_files.py
@@ -1,3 +1,10 @@
 import static_data
 
-static_data.names[0]
+def generate_reports(num):
+
+    for current_num in range(num):
+        # Constructing the file name
+        file_name = str(current_num + 1).zfill(3) + '_report.txt'   # zfill() populates with zero
+        print(file_name)
+
+generate_reports(15)
